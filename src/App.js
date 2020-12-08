33cd791d521ec4929792cd2fe225ce2e1c754886
izetta <rhobbie09@gmail.com>
2020-12-08 19:31:43 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './assets/scss/style.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LandingPage from 'elements/pages/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
