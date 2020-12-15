a24dda5c6c4d2feb549f297de85847925d7ef105
izetta <rhobbie09@gmail.com>
2020-12-15 22:54:12 +0700

    yang cukup membingungkan logika membuat stars nya dan sangat keren sekali tetap semangat, i trust u can end this project Rhoka

--- src/elements/pages/LandingPage.js
+++ src/elements/pages/LandingPage.js
@@ -3,17 +3,28 @@ import React, { Component } from 'react';
 import Header from 'parts/Header';
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked';
+import Categories from 'parts/Categories';
 
 import landingpage from 'json/landingPage';
+import Testimony from 'parts/Testimony';
 
 console.log(landingpage);
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingpage.hero} />
-        <MostPicked data={landingpage.mostPicked} />
+        <Hero refMostPicked={this.refMostPicked} data={landingpage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingpage.mostPicked}
+        />
+        <Categories data={landingpage.categories} />
+        <Testimony data={landingpage.testimonial} />
       </>
     );
   }
