29e5a99aa3392750f130b429fdf1a9e2969918d1
izetta <rhobbie09@gmail.com>
2021-01-05 22:21:51 +0700

    materi Front-end bwa telah selesai masih banyak yang mengganjjal untuk saya sendiri dan hari ini mengalami problem pages Checkout tidak muncul apa-apa bug kemarin masih belum saya perbaiki tetap semangat be Crazy Rhoka

---
+++ src/elements/Stepper/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import propTypes from 'prop-types';
+export default function Stepper(props) {
+  const { steps, initialStep } = props;
+  const stepsKey = Object.keys(steps);
+  const [CurrentStep, setCurrentStep] = useState(
+    stepsKey.indexOf(initialStep > -1 ? initialStep : steps[0])
+  );
+
+  const totalStep = stepsKey.length;
+  const indexStep = stepsKey.indexOf(CurrentStep);
+
+  function prevStep() {
+    if (+indexStep > 0) setCurrentStep(stepsKey[indexStep - 1]);
+  }
+
+  function nextStep() {
+    if (+indexStep < totalStep) setCurrentStep(stepsKey[indexStep + 1]);
+  }
+
+  return <>{props.children(prevStep, nextStep, CurrentStep, steps)}</>;
+}
+
+Stepper.propTypes = {
+  data: propTypes.object.isRequired,
+  initialStep: propTypes.string,
+};
+
+export { default as Numbering } from './Numbering';
+export { default as Meta } from './Meta';
+export { default as Controller } from './Controller';
+export { default as MainContent } from './MainContent';
